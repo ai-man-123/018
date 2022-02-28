e82372d9d3617a14af45f4b14a03337891e94a70
AIMAN *͘⁴̅⁰͍⁴̵〆* <82744566+ai-man-123@users.noreply.github.com>
2022-02-28 09:59:00 +0800

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,9 +1,9 @@
 const PhoneNumber = require('awesome-phonenumber')
 async function handler(m) {
-                let vcard = "BEGIN:VCARD\nVERSION:3.0\nN:1;ZaBot-MD;;;\nFN:ZaBot-MD\nitem1.TEL;waid=6281227144597:+62 812-2714-4597\nitem1.X-ABLabel:Ponsel\nX-WA-BIZ-NAME:ZaBot-MD\nEND:VCARD"
+                let vcard = "BEGIN:VCARD\nVERSION:3.0\nN:1;ZaBot-MD;;;\nFN:ZaBot-MD\nitem1.TEL;waid=60102810046:+60 10-281 0046\nitem1.X-ABLabel:Ponsel\nX-WA-BIZ-NAME:Aiman\nEND:VCARD"
                 conn.sendMessage(m.chat, { contacts: { contacts: [{ vcard }] } }, { quoted: m })
 }
-handler.help = ['owner', 'creator']
+handler.help = ['owner', 'Aiman']
 handler.tags = ['info']
 
 handler.command = /^(owner|creator)$/i
